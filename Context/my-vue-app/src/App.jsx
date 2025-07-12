306cc0d288dc469a247732312afb5728c4435c4b
Tanu chaturvedi <chaturveditanu975@gmail.com>
2025-07-12 22:56:17 +0530

    Update App.jsx component

--- Context/my-vue-app/src/App.jsx
+++ Context/my-vue-app/src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          <p>this is a paragraph</p>
           count is {count}
         </button>
         <p>
